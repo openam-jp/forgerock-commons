d169576b33ffbf27fe81c517babdf62667b768fb
Mark de Reeper <mark.dereeper@forgerock.com>
2016-08-22 10:58:10 +1200

    CHF-140 ContentTypeHeader should support additional parameters other than charset and boundary

--- http-framework/http-core/src/test/java/org/forgerock/http/header/ContentTypeHeaderTest.java
+++ http-framework/http-core/src/test/java/org/forgerock/http/header/ContentTypeHeaderTest.java
@@ -11,15 +11,20 @@
  * Header, with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions Copyright [year] [name of copyright owner]".
  *
- * Copyright 2014-2015 ForgeRock AS.
+ * Copyright 2014-2016 ForgeRock AS.
  */
 package org.forgerock.http.header;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.entry;
+import static org.assertj.core.api.Assertions.fail;
 import static org.forgerock.http.header.ContentTypeHeader.NAME;
 
 import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.TreeMap;
 
+import org.assertj.core.data.MapEntry;
 import org.forgerock.http.protocol.Message;
 import org.forgerock.http.protocol.Request;
 import org.forgerock.http.protocol.Response;
@@ -58,6 +63,7 @@ public class ContentTypeHeaderTest {
         assertThat(cth.getCharset()).isNull();
         assertThat(cth.getBoundary()).isNull();
         assertThat(cth.getValues()).isNullOrEmpty();
+        assertThat(cth.getAdditionalParameters()).isEmpty();
     }
 
     @Test
@@ -67,6 +73,7 @@ public class ContentTypeHeaderTest {
         assertThat(cth.getCharset()).isNull();
         assertThat(cth.getBoundary()).isNull();
         assertThat(cth.getValues()).isNullOrEmpty();
+        assertThat(cth.getAdditionalParameters()).isEmpty();
     }
 
     @Test(dataProvider = "contentTypeHeaderProvider")
@@ -76,6 +83,7 @@ public class ContentTypeHeaderTest {
         assertThat(cth.getType()).isEqualTo(type);
         assertThat(cth.getCharset()).isEqualTo(charset != null ? Charset.forName(charset) : null);
         assertThat(cth.getBoundary()).isEqualTo(boundary);
+        assertThat(cth.getAdditionalParameters()).isEmpty();
     }
 
     @Test(dataProvider = "contentTypeHeaderProvider")
@@ -87,6 +95,7 @@ public class ContentTypeHeaderTest {
         request.getHeaders().put(cth);
         assertThat(request.getHeaders().get(NAME)).isNotNull();
         assertThat(request.getHeaders().get(NAME).getValues()).containsOnly(cheader);
+        assertThat(cth.getAdditionalParameters()).isEmpty();
     }
 
     @Test
@@ -95,6 +104,7 @@ public class ContentTypeHeaderTest {
         assertThat(cth.getType()).isEqualTo(INVALID_CT_HEADER);
         assertThat(cth.getCharset()).isEqualTo(null);
         assertThat(cth.getBoundary()).isEqualTo(null);
+        assertThat(cth.getAdditionalParameters()).isEmpty();
     }
 
     @Test(dataProvider = "contentTypeHeaderProvider")
@@ -111,6 +121,7 @@ public class ContentTypeHeaderTest {
         assertThat(cth.getType()).isEqualTo(type);
         assertThat(cth.getCharset()).isEqualTo(charset != null ? Charset.forName(charset) : null);
         assertThat(cth.getBoundary()).isEqualTo(boundary);
+        assertThat(cth.getAdditionalParameters()).isEmpty();
     }
 
     @Test(dataProvider = "contentTypeHeaderProvider")
@@ -127,5 +138,75 @@ public class ContentTypeHeaderTest {
         assertThat(cth.getType()).isEqualTo(type);
         assertThat(cth.getCharset()).isEqualTo(charset != null ? Charset.forName(charset) : null);
         assertThat(cth.getBoundary()).isEqualTo(boundary);
+        assertThat(cth.getAdditionalParameters()).isEmpty();
+    }
+
+    @Test(expectedExceptions = NullPointerException.class)
+    public void testAdditionalParamsRejectNull() {
+        new ContentTypeHeader(null, null, null, null);
+        fail("Expecting NullPointerException");
+    }
+
+    @DataProvider
+    private Object[][] contentTypeHeaderProviderWithParams() {
+        return new Object[][]{
+            // content-type | type | charset | boundary | parameters
+            { "application/octet-stream; param=value", "application/octet-stream", null, null,
+                new MapEntry[]{ entry("param", "value") } },
+            { "application/octet-stream; param1=value1; param2=value2", "application/octet-stream", null, null,
+                new MapEntry[]{ entry("param1", "value1"), entry("param2", "value2") } },
+            { "application/octet-stream; boundary=gc0p4Jq0M2Yt08jU534c0p; param=value", "application/octet-stream",
+                null, "gc0p4Jq0M2Yt08jU534c0p", new MapEntry[]{ entry("param", "value") } },
+            { "application/octet-stream; boundary=gc0p4Jq0M2Yt08jU534c0p; param1=value1; param2=value2",
+                "application/octet-stream", null, "gc0p4Jq0M2Yt08jU534c0p",
+                new MapEntry[]{ entry("param1", "value1"), entry("param2", "value2") } },
+            // Params should have a value as-per the spec but allowing for compatibility with known use-cases.
+            { "application/octet-stream; emptyparam", "application/octet-stream", null, null,
+                new MapEntry[]{ entry("emptyparam", null) } },
+            { "application/octet-stream; emptyparam; param=value", "application/octet-stream", null, null,
+                new MapEntry[]{ entry("emptyparam", null), entry("param", "value") } },
+            { "application/octet-stream; boundary=gc0p4Jq0M2Yt08jU534c0p; emptyparam", "application/octet-stream",
+                null, "gc0p4Jq0M2Yt08jU534c0p", new MapEntry[]{ entry("emptyparam", null) } },
+            { "application/octet-stream; boundary=gc0p4Jq0M2Yt08jU534c0p; emptyparam; param=value",
+                "application/octet-stream", null, "gc0p4Jq0M2Yt08jU534c0p",
+                new MapEntry[]{ entry("emptyparam", null), entry("param", "value") } },
+            { "application/octet-stream; charset=UTF-8; boundary=gc0p4Jq0M2Yt08jU534c0p; emptyparam; param=value",
+                "application/octet-stream", "UTF-8", "gc0p4Jq0M2Yt08jU534c0p",
+                new MapEntry[]{ entry("emptyparam", null), entry("param", "value") } }
+        };
+    }
+
+    @Test(dataProvider = "contentTypeHeaderProviderWithParams")
+    public void testContentTypeHeaderWithParams(final String contentTypeHeader,
+                                                final String type,
+                                                final String charset,
+                                                final String boundary,
+                                                final MapEntry<String, String>[] additionalParameters) {
+        final ContentTypeHeader cth = ContentTypeHeader.valueOf(contentTypeHeader);
+
+        assertThat(cth.getType()).isEqualTo(type);
+        assertThat(cth.getCharset()).isEqualTo(charset != null ? Charset.forName(charset) : null);
+        assertThat(cth.getBoundary()).isEqualTo(boundary);
+        assertThat(cth.getAdditionalParameters()).containsOnly(additionalParameters);
+    }
+
+    @Test(dataProvider = "contentTypeHeaderProviderWithParams")
+    public void testSerializedValues(final String contentTypeHeader,
+                                     final String type,
+                                     final String charset,
+                                     final String boundary,
+                                     final MapEntry<String, String>[] additionalParameters) {
+        ContentTypeHeader cth = new ContentTypeHeader(type, charset, boundary, asMap(additionalParameters));
+
+        // Based on knowing the parameter names are stored in alphabetical order.
+        assertThat(cth.getValues()).containsOnly(contentTypeHeader);
+    }
+
+    private Map<String, String> asMap(MapEntry<String, String>[] toConvert) {
+        Map<String, String> result = new TreeMap<>();
+        for (MapEntry<String, String> entry : toConvert) {
+            result.put(entry.key, entry.value);
+        }
+        return result;
     }
 }
